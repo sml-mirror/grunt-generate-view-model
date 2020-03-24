113db88084f3433b77cf10745d24ddffeb3b38f1
n.khudkov <godba11@yandex.ru>
2020-03-24 15:49:05 +0500

    fix typing

--- index.d.ts
+++ index.d.ts
@@ -16,6 +16,7 @@ export declare class ViewModelTypeOptions {
     type: Object;
     modelName?: string;
     transformer?: Transformer;
+    nullable?: boolean;
 }
 
 export declare class Transformer {
